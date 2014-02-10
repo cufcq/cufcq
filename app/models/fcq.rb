ebc4b7693ba9cc5519423a2c9fd972ceaaa03167
saasbook <saasbook@saasbook.(none)>
2014-02-09 20:16:58 -0700

    updated fcq class

--- app/models/fcq.rb
+++ app/models/fcq.rb
@@ -1,8 +1,8 @@
 class Fcq < ActiveRecord::Base
-	validates :yearterm, length {is: 5}
-	validates :subject, length {is: 4}
-	validates :crse, length {is: 4}
-	validates :sec, length {maximum:  3} 
+	validates :yearterm, length: {is: 5}
+	validates :subject, length: {is: 4}
+	validates :crse, length: {is: 4}
+	validates :sec, length: {maximum:  3} 
 	validates :yearterm, :subject, :crse, :sec, :instructor_last, :instructor_first, :formsRequested, :formsReturned, :crsTitle, :campus, :college, :instr_Group, presence: true
 	def collected?
 		if(:formsReturned == 0)
