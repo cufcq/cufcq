7b1f43f8a69a96e62b38ca82f69f52601c53e9af
untra <untra.sam@gmail.com>
2014-02-10 15:38:59 -0700

    validator written

--- app/models/fcq.rb
+++ app/models/fcq.rb
@@ -1,2 +1,34 @@
+VALID_TERMS = {1 => "Spring",4 => "Summer",7=>"Fall"}
+VALID_GROUPS = ["TA", "TTT", "OTH"]
+class FcqValidator < ActiveModel::Validator
+  def validate(record)
+    if !VALID_TERMS.has_key?(record.yearterm % 10)
+      record.errors[:base] << "term is not valid! must end in 1, 4, 7 to indicate Spring, Summer and Fall respectively"
+    end
+    if !VALID_GROUPS.include?(record.instructor_group)
+      record.errors[:base] << "instructor group is not valid! must be either TTT, TA, or OTH"
+    end
+  end
+end
+
 class Fcq < ActiveRecord::Base
+  #validates the minimum entries are present
+  validates :yearterm, :subject, :crse, :sec, :instructor_last, :instructor_first, :forms_requested, :forms_returned, :course_title, :campus, :college, :instructor_group, presence: true
+  #validates entries match established patterns
+  validates :yearterm, length: { is: 5}
+  validates :subject, :crse, length: {is: 4}
+  validates :sec, length: {is: 3}
+  #validates instructor group is one of the specified types
+  #validates :instructor_group, inclusion: { in: %w(TA, TTT, OTH)}
+  #checks some more specific bits
+  validates_with FcqValidator
+
+  def float_passed
+    return percentage_passed.delete("%").to_f / 100
+  end
+
+  def uid
+    return "#{yearterm}#{subject}#{crse}#{sec}"
+  end
+
 end
