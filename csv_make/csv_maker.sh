0b1e17ce0533f9f39c1ad88e4fb8b4843a6f95a9
Alex Tsankov <antsankov@gmail.com>
2014-04-25 00:20:34 -0600

    yes

--- csv_make/csv_maker.sh
+++ csv_make/csv_maker.sh
@@ -12,3 +12,5 @@ tail -n +2 "$output" > work.csv
 echo 'yearterm,subject,crse,sec,onlineFCQ,bd_continuing_education,instructor_last,instructor_first,forms_requested,forms_returned,percentage_passed,course_overall,course_overall_SD,instructor_overall,instructor_overall_SD,total_hours,prior_interest,effectiveness,availability,challenge,amount_learned,respect,course_title,courseOverall_old,courseOverall_SD_old,instrOverall_old,instrOverall_SD_old,r_Fair,r_Access,workload,r_Divstu,r_Diviss,r_Presnt,r_Explan,r_Assign,r_Motiv,r_Learn,r_Complx,campus,college,aSdiv,level,fcqdept,instructor_group,i_Num' | cat - work.csv > temp.csv 
 
 awk -F, '/,/{gsub(/ /, "", $0); print}' temp.csv > $output
+rm temp.csv
+rm work.csv
