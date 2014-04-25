aa67efaa4ac96b5536846e78453a1fd890db6bb4
saasbook <saasbook@saasbook.(none)>
2014-04-24 23:21:12 -0600

    Worked on CSV

--- csv_make/csv_maker.sh
+++ csv_make/csv_maker.sh
@@ -1,10 +1,12 @@
-echo "Enter input file (include location): "
+#!/bin/bash
+echo "Enter input file XCEL (include location): "
 read input
 echo "Enter output file (include location): "
 read output
 
 ssconvert -S $input temp.csv
+#converts 
 tr -d '"' < *.csv.1 | sed 's/Instructor,/Instructor_last,Instructor_first,/g' > $output
 rm -r *.csv.?
-tail -n +2 "$output"
-echo 'yearterm,subject,crse,sec,onlineFCQ,bDContinEdCrse,instructor_last,instructor_first,formsRequested,formsReturned,courseOverallPctValid,courseOverall,courseOverall_SD,instructorOverall,instructorOverall_SD,hoursPerWkInclClass,priorInterest,instrEffective,availability,challenge,howMuchLearned,instrRespect,crsTitle,courseOverall_old,courseOverall_SD_old,instrOverall_old,instrOverall_SD_old,r_Fair,r_Access,workload,r_Divstu,r_Diviss,r_Presnt,r_Explan,r_Assign,r_Motiv,r_Learn,r_Complx,campus,college,aSdiv,level,fcqdept,instr_Group,i_Num' | cat -$output > temp && mv temp $output
+tail -n +2 "$output" > work.csv
+echo 'yearterm,subject,crse,sec,onlineFCQ,bDContinEdCrse,instructor_last,instructor_first,forms_requested,forms_returned,courseOverallPctValid,courseOverall,courseOverall_SD,instructorOverall,instructorOverall_SD,hoursPerWkInclClass,priorInterest,instrEffective,availability,challenge,howMuchLearned,instrRespect,course_title,courseOverall_old,courseOverall_SD_old,instrOverall_old,instrOverall_SD_old,r_Fair,r_Access,workload,r_Divstu,r_Diviss,r_Presnt,r_Explan,r_Assign,r_Motiv,r_Learn,r_Complx,campus,college,aSdiv,level,fcqdept,instructor_group,i_Num' | cat - work.csv > temp && mv temp $output
