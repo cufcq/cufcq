ea5d5b95ffe7133e1198cd2702c045faf3aeb032
Alexander Tsankov <antsankov@gmail.com>
2015-01-03 10:55:52 -0700

    Polished the scraper

--- csv_make/scraper.py
+++ csv_make/scraper.py
@@ -8,78 +8,79 @@ br.set_handle_robots(False)   # ignore robots
 br.set_handle_refresh(False)  # can sometimes hang without this
 br.addheaders = [('User-agent', 'Firefox')]
 
-#the url for the fcq site
-url = 'https://fcq.colorado.edu/UCBdata.htm'
-
-#we open the url 
-response = br.open(url)
-
-# NOTE: Use this to find all available forms on a page 
-# for form in br.forms():
-#     print "Form name:", form.name
-#     print form
-
-# find the proper form 
-control = br.select_form("frmFCQ")
-
-
 fcqdpt = 'BD : Entire Campus ## BD'
 # 1 is Spring, 4 is Summer, 7 is Fall  
 ftrm = '1'
+ltrm = '1'
 fileFrmt = 'XLS'
-f
+# First year: fyr=[2014, 2013, 2012, 2011, 2010, 2009, 2008, *2007]
+fyrs = ['2013', '2012', '2011', '2010', '2009', '2008', '2007']
+lyrs = ['2014', '2013', '2012', '2011', '2010', '2009', '2008']
+# The instructor group grp1=[*ALL, TTT, OTH, T_O, TA]
+grp1 = 'ALL'
 
 
-#go through all of the form options so we can change them 
-for control in br.form.controls:
-	#this will show us all of our default value for the fields. See page options for the output of the FCQ page as of 1/3/15
-    #print "DEFAULT: type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
-	
-	#this is our fcq department 
-    if (control.name == 'fcqdpt'):
-    	br[control.name] = ['AS : POLITICAL SCIENCE -- PSCI']
-    	print "CHANGE fcqdpt type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+for i in range (0,len(fyrs)):
 
-    #this is our first term
-    if (control.name == 'ftrm'):
-    	br[control.name] = ['1']
-    	print "CHANGE first term type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+	#the url for the fcq site
+	url = 'https://fcq.colorado.edu/UCBdata.htm'
 
-    if (control.name == 'ltrm'):
-    	br[control.name] = ['1']
-    	print "CHANGE last term type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+	#we open the url 
+	response = br.open(url)
 
-    if (control.name == 'fileFrmt'):
-    	br[control.name] = ['XLS']
-    	print "CHANGE fileFrmt type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+	control = br.select_form("frmFCQ")
 
-    if (control.name == 'fyr'):
-    	br[control.name] = ['2008']
-    	print "CHANGE first year type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+	#go through all of the form options so we can change them 
+	for control in br.form.controls:
+		#this will show us all of our default value for the fields. See page options for the output of the FCQ page as of 1/3/15
+	    #print "DEFAULT: type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+		
+	    if (control.name == 'fcqdpt'):
+	    	br[control.name] = [fcqdpt]
+	    	print "CHANGE fcqdpt type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
 
-    if (control.name == 'lyr'):
-    	br[control.name] = ['2009']
-    	print "CHANGE first year type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+	    elif (control.name == 'ftrm'):
+	    	br[control.name] = [ftrm]
+	    	print "CHANGE first term type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
 
-response = br.submit()
+	    elif (control.name == 'ltrm'):
+	    	br[control.name] = [ltrm]
+	    	print "CHANGE last term type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
 
-if 'Currently, Excel files with FCQ results are limited to 50,000 rows.' in response.read():
-	print('CAUTION REQUEST HAS MORE THAN 50,000 LINES!')
+	    elif (control.name == 'fileFrmt'):
+	    	br[control.name] = [fileFrmt]
+	    	print "CHANGE fileFrmt type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
 
-for link in br.links():
-    if (link.text == 'Click here'):
-		print(link.url)
-		original = link.url
-		#need to split because output link is: javascript:popup('/fcqtemp/BD010395426.xls','BD010395426','750','550','yes','yes')
-		split = original.replace("\'","").split(',')
-		#get this BD010395426
-		proper_link = "https://fcq.colorado.edu/fcqtemp/%s.xls" % (split[1])
-		print proper_link
+	    elif (control.name == 'fyr'):
+	    	br[control.name] = [fyrs[i]]
+	    	print "CHANGE first year type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
 
-r = requests.get(proper_link)
+	    elif (control.name == 'lyr'):
+	    	br[control.name] = [lyrs[i]]
+	    	print "CHANGE last year type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
 
-output = open('test.xls','wb')
-output.write(r.content)
-output.close()
+	response = br.submit()
 
+	if 'Currently, Excel files with FCQ results are limited to 50,000 rows.' in response.read():
+		print('CAUTION REQUEST HAS MORE THAN 50,000 LINES!')
+
+	for link in br.links():
+	    if (link.text == 'Click here'):
+			print(link.url)
+			original = link.url
+			#need to split because output link is: javascript:popup('/fcqtemp/BD010395426.xls','BD010395426','750','550','yes','yes')
+			split = original.replace("\'","").split(',')
+			#get this BD010395426
+			proper_link = "https://fcq.colorado.edu/fcqtemp/%s.xls" % (split[1])
+			print proper_link
+
+	r = requests.get(proper_link)
+
+	fileName = "raw/{ftrm}:{fyr}_{ltrm}:{lyr}.xls".format(fcqdpt=fcqdpt,ftrm=ftrm,fyr=fyrs[i],ltrm=ltrm,lyr=lyrs[i])
+	print(fileName)
+	output = open(fileName,'wb')
+	output.write(r.content)
+	output.close()
+
+print("SUCCESS!")
 
