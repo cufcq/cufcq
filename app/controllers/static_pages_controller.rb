ba4da53e015d3d2d925b1b051d91137ebac94c0a
Alexander Tsankov <antsankov@gmail.com>
2015-01-27 13:39:25 -0700

    Got search to work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,18 @@
 class StaticPagesController < ApplicationController
   def home
- 	if params[:search].include? "instructor"
-      puts "YYYYYYYYYYYY " + params[:search]
-    end
+		# the name to search for params[:search]
+  	# the options (insturctors/courses/dept) params[:option]
+
+  	# /?utf8=%E2%9C%93&option=%2Finstructors&search=george
+		
+		#checks if there is something in the search box, if there is it redirect to a custom url make from the option and the search params!
+		if params[:search] != ''
+			redirect_to params[:option] + '?utf8=%E2%9C%93&search=' +  params[:search]
+		end 
+  end
+
+  def search
+  	raise 'foo'
   end
 
   def help
