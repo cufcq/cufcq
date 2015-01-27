a01650042c0cfb9b340cb02f603bf447a6114e36
untra <untra.sam@gmail.com>
2015-01-26 23:29:40 -0700

    yeah, fixed that

--- app/models/course.rb
+++ app/models/course.rb
@@ -61,27 +61,53 @@ class Course < ActiveRecord::Base
     return set.average(:course_overall).round(1)
   end
 
-  attr_reader :semesters, :overall_data, :challenge_data, :interest_data, :learned_data, :categories
+  attr_reader :semesters, :overall_data, :challenge_data, :interest_data, :learned_data, :grade_data, :categories, :pct_a_data, :pct_b_data, :pct_c_data, :pct_d_data, :pct_f_data, :pct_i_data
 
   def overall_query
     overalls = self.fcqs.where.not(instructor_group: 'TA').group("yearterm").average(:course_overall)
     challenge = self.fcqs.where.not(instructor_group: 'TA').group("yearterm").average(:challenge)
     interest = self.fcqs.where.not(instructor_group: 'TA').group("yearterm").average(:prior_interest)
     learned = self.fcqs.where.not(instructor_group: 'TA').group("yearterm").average(:amount_learned)
+    grade = self.fcqs.where.not(instructor_group: 'TA').group("yearterm").average(:avg_grd)
     @semesters = []
     @overall_data = []
     @challenge_data = [] 
     @interest_data = [] 
-    @learned_data = [] 
+    @learned_data = []
+    @grade_data = [] 
     #records.each {|k,v| fixedrecords[Fcq.semterm_from_int(k)] = v.to_f.round(1)}
     overalls.each {|k,v| @overall_data << [k,v.to_f.round(1)]}
     challenge.each {|k,v| @challenge_data << [k,v.to_f.round(1)]}
     interest.each {|k,v| @interest_data << [k,v.to_f.round(1)]}
     learned.each {|k,v| @learned_data << [k,v.to_f.round(1)]}
+    grade.each {|k,v| @grade_data << [k,v.to_f.round(2)]}
     #if any of the data is < 1.0, it marks it with an x marker
     puts overall_data
     #@chart_data = fixedrecords.values
     puts @chart_data
+  end
+
+
+  def grade_query
+    pct_a = self.fcqs.where.not(instructor_group: 'TA').group("yearterm").average(:pct_a)
+    pct_b = self.fcqs.where.not(instructor_group: 'TA').group("yearterm").average(:pct_b)
+    pct_c = self.fcqs.where.not(instructor_group: 'TA').group("yearterm").average(:pct_c)
+    pct_d = self.fcqs.where.not(instructor_group: 'TA').group("yearterm").average(:pct_d)
+    pct_f = self.fcqs.where.not(instructor_group: 'TA').group("yearterm").average(:pct_f)
+    pct_i = self.fcqs.where.not(instructor_group: 'TA').group("yearterm").average(:pct_incomp)
+    @semesters = []
+    @pct_a_data = []
+    @pct_b_data = []
+    @pct_c_data = []
+    @pct_d_data = []
+    @pct_f_data = []
+    @pct_i_data = []
+    pct_a.each {|k,v| @pct_a_data << [k,v.to_f.round(1)]}
+    pct_b.each {|k,v| @pct_b_data << [k,v.to_f.round(1)]}
+    pct_c.each {|k,v| @pct_c_data << [k,v.to_f.round(1)]}
+    pct_d.each {|k,v| @pct_d_data << [k,v.to_f.round(1)]}
+    pct_f.each {|k,v| @pct_f_data << [k,v.to_f.round(1)]}
+    pct_i.each {|k,v| @pct_i_data << [k,v.to_f.round(1)]}
   end
 
 
