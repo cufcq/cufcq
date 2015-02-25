a1377ca6fa62afa656dc417af5af9fad48c5d8e8
cufcq <untra.sam@gmail.com>
2015-02-25 21:18:47 +0000

    Removed old scripts, tests, and fixed gitignore

--- launcher.sh
+++ launcher.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-ps -ef | grep solr | grep -v grep | awk '{print $2}' | xargs kill -9
 
-#to confirm solr is killed 
-ps aux | grep solr
-
-rm solr/pids/production/sunspot-solr-production.pid
-
-# start the server in the development environement 
-# WARNING: when launching on a production server, MANUALLY specifiy the production env. 
-
-#http://stackoverflow.com/questions/7305709/ruby-on-rails-solr-sunspot-connection-refused-connect2
-
+echo "Starting/Reindexing Solr"
 rake sunspot:solr:start RAILS_ENV=development
 rake sunspot:solr:reindex RAILS_ENV=development
+
+echo "Solr successful! Starting Rails" 
 
 rails s
 echo "rails server closed!"
--- spec/support/utilities.rb
+++
@@ -1,8 +0,0 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-        base_title
-          else
-              "#{base_title} | #{page_title}"
-                end
-                end
