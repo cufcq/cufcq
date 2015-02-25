ff3ae05846228f8de81af9eeb8587077e9310149
cufcq <untra.sam@gmail.com>
2015-02-25 21:03:28 +0000

    Fixed total reload script,and added to gitignore

--- total_reload.sh
+++ total_reload.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-ps -ef | grep solr | grep -v grep | awk '{print $2}' | xargs
 
-#to confirm solr is killed·
-ps aux | grep solr
-
-rm solr/pids/production/sunspot-solr-production.pid
-
-#This does a total reload, it includes all of the populate tasks. 
-#See lib/tasks/import.rake for more info
+#kill solr process
 pkill -f solr
+
+#remove old data
+rm -rf solr/data
+rm -rf solr/default
+rm -rf solr/development
+rm -rf solr/pids
+rm -rf solr/test
+
+#startup solr in development environment
 rake sunspot:solr:start RAILS_ENV=development
 
+#all of our rake tasks
 bundle exec rake db:reset
 bundle exec rake db:migrate
 bundle exec rake departments
 bundle exec rake import
-# bundle exec rake instructor_populate
-# bundle exec rake course_populate
-# bundle exec rake department_populate
 bundle exec rake recitation_correction
 bundle exec rake ic_relations
 bundle exec rake grades
+
+#reindex solr 
 RAILS_ENV=development bundle exec rake sunspot:solr:reindex
