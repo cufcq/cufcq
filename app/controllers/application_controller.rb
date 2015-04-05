e5b4d61925ee38a78b85ca9e213a50805e81da16
untra <untra.sam@gmail.com>
2015-04-05 10:34:16 -0600

    Riley's addition to the website being added. Changed background from mountains to a gradient. Added in a few nice css elements to the homepage. Will finish later.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-    before_filter :prepare_for_mobile
+  # before_filter :prepare_for_mobile
 
-private
+# private
 
-    def mobile_device?
-      if session[:mobile_param]
-        session[:mobile_param] == "1"
-      else
-        request.user_agent =~ /Mobile|webOS/
-      end
-    end
-    helper_method :mobile_device?
+#     def mobile_device?
+#       if session[:mobile_param]
+#         session[:mobile_param] == "1"
+#       else
+#         request.user_agent =~ /Mobile|webOS/
+#       end
+#     end
+#     helper_method :mobile_device?
     
-    def prepare_for_mobile
-      session[:mobile_param] = params[:mobile] if params[:mobile]
-      request.format = :mobile if mobile_device?
-    end
+#     def prepare_for_mobile
+#       # session[:mobile_param] = params[:mobile] if params[:mobile]
+#       # request.format = :mobile if mobile_device?
+#     end
 
  #Can we just Create and index method and pass it along to all of the other controllers via inheritence?
 #   def index
