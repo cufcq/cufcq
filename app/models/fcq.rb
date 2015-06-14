fbd5845ac258d5705ed82771d13cb989fd8318b4
untra <untra.sam@gmail.com>
2015-06-13 18:13:50 -0600

    cleanup looking nice. fcqs done. Will totalreload and check the site. If things look right, will push.

--- app/models/fcq.rb
+++ app/models/fcq.rb
@@ -1,226 +1,147 @@
-VALID_TERMS = {1 => "Spring",4 => "Summer",7=>"Fall"}
-VALID_GROUPS = ["TA", "TTT", "OTH"]
+VALID_TERMS = { 1 => 'Spring', 4 => 'Summer', 7 => 'Fall' }
+VALID_GROUPS = ['TA', 'TTT', 'OTH']
 class FcqValidator < ActiveModel::Validator
   def validate(record)
     key = record.yearterm || 0
-    # puts key
     key %= 10
-    if !VALID_TERMS.has_key?(key)
-      record.errors[:base] << "term is not valid! must end in 1, 4, 7 to indicate Spring, Summer and Fall instrrespectively"
+    unless VALID_TERMS.key?(key)
+      record.errors[:base] << 'term is not valid! must end in 1, 4, 7 to indicate Spring, Summer and Fall respectively'
     end
-    if !VALID_GROUPS.include?(record.instr_group)
-      record.errors[:base] << "instructor group is not valid! must be either TTT, TA, or OTH"
+    unless VALID_GROUPS.include?(record.instr_group)
+      record.errors[:base] << 'instructor group is not valid! must be either TTT, TA, or OTH'
     end
   end
 end
 
 class Fcq < ActiveRecord::Base
-  #Belongs to
+  # Belongs to
   belongs_to :instructor, counter_cache: true
   belongs_to :course, counter_cache: true
   belongs_to :department, counter_cache: true
-  #validates the minimum entries are present
+  # validates the minimum entries are present
   validates :yearterm, :subject, :crse, :sec, :instructor_last, :instructor_first, :formsrequested, :formsreturned, :campus, :college, :instr_group, presence: true
-  #validates entries match established patterns
-  validates :yearterm, length: { is: 5}
-  validates :subject, :crse, length: {is: 4}
-  validates :sec, length: {maximum: 3}
-  #validates instructor group is one of the specified types
-  #validates :instr_group, inclusion: { in: %w(TA, TTT, OTH)}
-  #checks some more specific bits
+  # validates entries match established patterns
+  validates :yearterm, length: { is: 5 }
+  validates :subject, :crse, length: { is: 4 }
+  validates :sec, length: { maximum: 3 }
   validates_with FcqValidator
-  #validates uniqueness
-  #validates :yearterm, uniqueness: true
   validates_uniqueness_of :sec, scope: [:crse, :subject, :yearterm, :instructor_last, :instructor_first]
   # before_save :update_counters
   ##################################
 
   def pass_rate
-    return percentage_passed_string
+    percentage_passed_string
   end
 
   def float_passed
     failed = pct_c_minus_or_below || 1.0
-    fp = (1.0 - failed)
-    return fp
+    (1.0 - failed)
   end
 
   def percentage_passed_string
-  	val = (float_passed * 100).round(0)
-  	val = [val, 100].min
-  	val = [val, 0].max
-  	if (val == 0)
-  		return '--'
-  	else
-  		string = val.round
-  		return "#{string}%"
-  	end
-  end 
+    val = (float_passed * 100).round(0)
+    val = [val, 100].min
+    val = [val, 0].max
+    return '--' if (val == 0)
+    "#{val.round}%"
+  end
 
   def pct_a_string
-    if pct_a != nil
+    unless pct_a.nil?
       val = (pct_a * 100).round(0)
       val = [val, 100].min
       val = [val, 0].max
-      if (val == 0)
-        return '--'
-      else
-        string = val.round
-        return "#{string}%"
-      end
-    else 
-      return "--"
-    end 
-  end 
+      return '--' if (val == 0)
+      return "#{val.round}%"
+    end
+    '--'
+  end
 
   def pct_b_string
-    if pct_a != nil
+    unless pct_b.nil?
       val = (pct_b * 100).round(0)
       val = [val, 100].min
       val = [val, 0].max
-      if (val == 0)
-        return '--'
-      else
-        string = val.round
-        return "#{string}%"
-      end
-    else 
-      return "--"
-    end 
-  end 
+      return '--' if (val == 0)
+      return "#{val.round}%"
+    end
+    '--'
+  end
 
   def pct_c_string
-    if pct_c != nil
+    unless pct_c.nil?
       val = (pct_c * 100).round(0)
       val = [val, 100].min
       val = [val, 0].max
-      if (val == 0)
-        return '--'
-      else
-        string = val.round
-        return "#{string}%"
-      end
-    else 
-      return "--"
-    end 
-  end 
+      return '--' if (val == 0)
+      return "#{val.round}%"
+    end
+    '--'
+  end
 
   def pct_d_string
-    if pct_d != nil
+    unless pct_d.nil?
       val = (pct_d * 100).round(0)
       val = [val, 100].min
       val = [val, 0].max
-      if (val == 0)
-        return '--'
-      else
-        string = val.round
-        return "#{string}%"
-      end
-    else 
-      return "--"
-    end 
+      return '--' if (val == 0)
+      return "#{val.round}%"
+    end
+    '--'
   end
 
   def pct_f_string
-    if pct_f != nil
+    unless pct_f.nil?
       val = (pct_f * 100).round(0)
       val = [val, 100].min
       val = [val, 0].max
-      if (val == 0)
-        return '--'
-      else
-        string = val.round
-        return "#{string}%"
-      end
-    else 
-      return "--"
-    end 
-  end
-
-  def avg_grd_string
-    if avg_grd != nil
-      val = avg_grd.round(2)
-      return "#{val}"
-    else
-      return "--"
+      return '--' if (val == 0)
+      return "#{val.round}%"
     end
+    '--'
   end
-
 
   ###################################
 
-  def course_overall_string 
-    if courseoverall != nil
-      val = courseoverall.round(1)
-      return "#{val} / 6.0"
-    else 
-      return "--"
-    end
+  def course_overall_string
+    return "#{courseoverall.round(1)} / 6.0" unless courseoverall.nil?
+    '--'
   end
 
-  def instructor_overall_string 
+  def instructor_overall_string
     if instructoroverall != nil
       val = instructoroverall.round(1)
       return "#{val} / 6.0"
-    else 
-      return "--"
+    else
+      return '--'
     end
   end
 
   ##################################
- 
+
   def department_name_string
-    if department != nil 
-      val = department.name
-      return "#{val} / 6.0"
-    else 
-      return "--"
-    end
+    return "#{department.name} / 6.0" unless department.nil?
+    '--'
   end
 
   def challenge_string
-    if challenge != nil 
-      val = challenge.round(1)
-      return "#{val} / 6.0"
-    else 
-      return "--"
-    end
+    return "#{challenge.round(1)} / 6.0" unless challenge.nil?
+    '--'
   end
 
   def prior_interest_string
-    if priorinterest != nil 
-      val = priorinterest.round(1)
-      return "#{val} / 6.0"
-    else 
-      return "--"
-    end
+    return "#{priorinterest.round(1)} / 6.0" unless priorinterest.nil?
+    '--'
   end
 
   def howmuchlearned_string
-    if howmuchlearned != nil 
-      val = howmuchlearned.round(1)
-      return "#{val} / 6.0"
-    else 
-      return "--"
-    end
-  end
-
-  def howmuchlearned_string
-    if howmuchlearned != nil 
-      val = howmuchlearned.round(1)
-      return "#{val} / 6.0"
-    else 
-      return "--"
-    end
+    return "#{howmuchlearned.round(1)} / 6.0" unless howmuchlearned.nil?
+    '--'
   end
 
   def avg_grd_string
-    if avg_grd != nil 
-      val = avg_grd.round(2)
-      return "#{val} / 4.0"
-    else 
-      return "--"
-    end
+    return "#{avg_grd.round(2)} / 4.0" unless avg_grd.nil?
+    '--'
   end
 
   def yearterm_identifier
@@ -228,37 +149,32 @@ class Fcq < ActiveRecord::Base
   end
 
   def section_string
-    return sec.to_s.rjust(3, '0')
+    sec.to_s.rjust(3, '0')
   end
 
   def collected_online
-    if onlinefcq == "OL"
-      return true
-    end
-    return false
+    return true if onlinefcq == 'OL'
+    false
   end
 
   def uid
-    return "#{yearterm}#{subject}#{crse}#{sec}"
+    "#{yearterm}#{subject}#{crse}#{sec}"
   end
 
   def n_withdrawn
     e = n_enroll || 0
     t = n_eot || 0
-    return [(e - t), 0].max
+    [(e - t), 0].max
   end
 
   def title
-    if corrected_course_title != nil
-      return corrected_course_title
-    else
-      return capitalized_title
-    end
+    return corrected_course_title unless corrected_course_title.nil?
+    capitalized_title
   end
 
   def capitalized_title
-    ctitle =  course_title || course.course_title ||""
-    return ctitle.split.map(&:capitalize).join(' ')
+    ctitle = course_title || course.course_title || ''
+    ctitle.split.map(&:capitalize).join(' ')
   end
 
   def correct_title(title)
@@ -266,118 +182,91 @@ class Fcq < ActiveRecord::Base
   end
 
   def recitation?
-    if (course_title == "REC" || course_title == "RECITATION")
-      return true
-    elsif (activity_type[0..2] == "LEC" && instr_group == "TA")
-      return true
-    elsif (activity_type != nil)
-      if (activity_type[0..2] == "REC")
-        return true
-      else
-        return false
-      end
-    else 
-      return true
+    return true if (course_title == 'REC' || course_title == 'RECITATION')
+    return true if (activity_type[0..2] == 'LEC' && instr_group == 'TA')
+    unless activity_type.nil?
+      return true if (activity_type[0..2] == 'REC')
+      return false
     end
+    true
   end
 
   def activity_type_string
-    if(activity_type != nil)
-      return activity_type
-    else
-      return "--"
-    end
+    activity_type unless activity_type.nil?
+    '--'
   end
 
   def hours_string
-    if(hours != nil)
-      return self.hours
-    else
-      return "--"
-    end
+    hours unless hours.nil?
+    '--'
   end
 
   def ld?
-    return (crse < 3000)
+    (crse < 3000)
   end
 
   def ud?
-    return !(self.ld?) && (crse < 5000)
+    !(self.ld?) && (crse < 5000)
   end
 
   def grad?
-    return (crse >= 5000)
+    (crse >= 5000)
   end
 
   def rank_string
-    if ld?
-      return "Lower Division"
-    elsif ud?
-      return "Upper Division"
-    else
-      return "Graduate Level"
-    end
+    return 'Lower Division' if ld?
+    return 'Upper Division' if ud?
+    'Graduate Level'
   end
 
   def rank_string_abridged
-    if ld?
-      return "ld"
-    elsif ud?
-      return "ud"
-    else
-      return "gd"
-    end
+    return 'ld' if ld?
+    return 'ud' if ud?
+    'gd'
   end
 
   def year
-    return yearterm / 10
+    yearterm / 10
   end
 
   attr_reader :semterm
   def semterm
-    return (VALID_TERMS[yearterm % 10] + " " + year.to_s)
+    (VALID_TERMS[yearterm % 10] + ' ' + year.to_s)
   end
 
   def self.semterm_from_int(s)
-    return (VALID_TERMS[s % 10] + " " + (s/10).to_s)
+    (VALID_TERMS[s % 10] + ' ' + (s / 10).to_s)
   end
 
   def summer_fcq?
-    return ((yearterm % 10) == 4)
+    ((yearterm % 10) == 4)
   end
 
   def bad?
-    if missing_fcq_data?
-      return true
-    end
-    return (formsreturned < 1)
+    return true if missing_fcq_data?
+    (formsreturned < 1)
   end
 
   def missing_fcq_data?
-    return (courseoverall == nil)
+    (courseoverall.nil?)
   end
 
-
   def missing_grade_data?
-    return (avg_grd == nil)
+    avg_grd.nil?
   end
 
   def color
-    if ld?
-      return "box1"
-    elsif ud?
-      return "box2"
-    else
-      return "box3"
-    end
+    return 'box1' if ld?
+    return 'box2' if ud?
+    'box3'
   end
 
   def fcq_object
-    return %Q{#{semterm} #{subject} #{crse}-#{sec} #{title}}
+    "#{semterm} #{subject} #{crse}-#{sec} #{title}"
   end
 
   def fcq_header
-    return "#{crse}-#{sec}"
+    "#{crse}-#{sec}"
   end
 
   def requested_returned_string
@@ -387,5 +276,4 @@ class Fcq < ActiveRecord::Base
   def instructor_full_name
     "#{instructor_first} #{instructor_last}"
   end
-
 end
